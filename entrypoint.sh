e901312363bd36fc5b652aff8fc844d0fb00515d
Christopher Johnstone <meson800@gmail.com>
2023-11-19 12:30:03 -0500

    Set windowsize every minute and take screenshots every minute

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,10 @@
 if [[ $# -eq 0 ]] ; then
     /usr/bin/X :0 -nolisten tcp vt1
 else
+    mkdir -p /screenshots
     # maximize the main window
-    echo 'nohup bash -c "sleep 10 ; xdotool getwindowfocus windowsize $(xdotool getdisplaygeometry)" &' > /usr/bin/entrypoint-command.sh
+    echo 'nohup bash -c "sleep 10; while true; do xdotool getwindowfocus windowsize $(xdotool getdisplaygeometry); sleep 60; done" &' > /usr/bin/entrypoint-command.sh
+    echo 'nohup bash -c "sleep 10; while true; do scrot -F /screenshots/screen.png -o -z; sleep 60; done" &' >> /usr/bin/entrypoint-command.sh
 
     echo "$@" >> /usr/bin/entrypoint-command.sh
     chmod +x /usr/bin/entrypoint-command.sh
