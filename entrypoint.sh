58d55fa28f5412fd57c2638da532819fd0b208c3
YashSam1 <130311450+YashSam1@users.noreply.github.com>
2024-06-02 18:19:58 -0400

    Update entrypoint.sh
    Fixing location of screenshots folder

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,10 +3,10 @@
 if [[ $# -eq 0 ]] ; then
     /usr/bin/X :0 -nocursor -nolisten tcp vt1
 else
-    mkdir -p /screenshots
+    mkdir -p tmp/screenshots
     # maximize the main window
     echo 'nohup bash -c "sleep 10; while true; do xdotool getwindowfocus windowsize $(xdotool getdisplaygeometry); sleep 60; done" &' > /usr/bin/entrypoint-command.sh
-    echo 'nohup bash -c "sleep 10; while true; do scrot -F /screenshots/screen.png -o -z; sleep 60; done" &' >> /usr/bin/entrypoint-command.sh
+    echo 'nohup bash -c "sleep 10; while true; do scrot -F tmp/screenshots/screen.png -o -z; sleep 60; done" &' >> /usr/bin/entrypoint-command.sh
 
     echo "$@" >> /usr/bin/entrypoint-command.sh
     chmod +x /usr/bin/entrypoint-command.sh
