61fd83e63cf753ed78e75469c80d405d87e81322
YashSam1 <130311450+YashSam1@users.noreply.github.com>
2024-06-29 12:55:27 -0400

    Update entrypoint.sh
    Try logging scrot output

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 if [[ $# -eq 0 ]] ; then
-    /usr/local/bin/X :0 -nocursor -nolisten tcp vt1
+    /usr/bin/X :0 -nocursor -nolisten tcp vt1
 else
     mkdir -p tmp/screenshots
     # maximize the main window
     echo 'nohup bash -c "sleep 10; while true; do xdotool getwindowfocus windowsize $(xdotool getdisplaygeometry); sleep 60; done" &' > /usr/local/bin/entrypoint-command.sh
-    echo 'nohup bash -c "sleep 10; while true; do scrot -F tmp/screenshots/screen.png -o -z; sleep 60; done" &' >> /usr/local/bin/entrypoint-command.sh
+    echo 'nohup bash -c "sleep 10; while true; do scrot -F tmp/screenshots/screen.png -o -z >> tmp/screenshots/scrot.log 2>&1; sleep 60; done" &' >> /usr/local/bin/entrypoint-command.sh
 
-    echo "$@" >> /usr/local/bin/entrypoint-command.sh
-    chmod +x /usr/local/bin/entrypoint-command.sh
-    /usr/local/bin/xinit /usr/local/bin/entrypoint-command.sh -- :0 -nolisten tcp vt1
+    echo "$@" >> /usr/bin/entrypoint-command.sh
+    chmod +x /usr/bin/entrypoint-command.sh
+    /usr/bin/xinit /usr/bin/entrypoint-command.sh -- :0 -nolisten tcp vt1
 fi
