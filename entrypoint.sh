8babc6fcb6e417e71e01a7e69249375278085c9e
YashSam1 <130311450+YashSam1@users.noreply.github.com>
2024-06-29 11:31:33 -0400

    Update entrypoint.sh
    Changing usr/bin to usr/local/bin

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 if [[ $# -eq 0 ]] ; then
-    /usr/bin/X :0 -nocursor -nolisten tcp vt1
+    /usr/local/bin/X :0 -nocursor -nolisten tcp vt1
 else
     mkdir -p tmp/screenshots
     # maximize the main window
-    echo 'nohup bash -c "sleep 10; while true; do xdotool getwindowfocus windowsize $(xdotool getdisplaygeometry); sleep 60; done" &' > /usr/bin/entrypoint-command.sh
-    echo 'nohup bash -c "sleep 10; while true; do scrot -F tmp/screenshots/screen.png -o -z; sleep 60; done" &' >> /usr/bin/entrypoint-command.sh
+    echo 'nohup bash -c "sleep 10; while true; do xdotool getwindowfocus windowsize $(xdotool getdisplaygeometry); sleep 60; done" &' > /usr/local/bin/entrypoint-command.sh
+    echo 'nohup bash -c "sleep 10; while true; do scrot -F tmp/screenshots/screen.png -o -z; sleep 60; done" &' >> /usr/local/bin/entrypoint-command.sh
 
-    echo "$@" >> /usr/bin/entrypoint-command.sh
-    chmod +x /usr/bin/entrypoint-command.sh
-    /usr/bin/xinit /usr/bin/entrypoint-command.sh -- :0 -nolisten tcp vt1
+    echo "$@" >> /usr/local/bin/entrypoint-command.sh
+    chmod +x /usr/local/bin/entrypoint-command.sh
+    /usr/local/bin/xinit /usr/local/bin/entrypoint-command.sh -- :0 -nolisten tcp vt1
 fi
