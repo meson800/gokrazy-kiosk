459375c8ede532d933f11ca1559af0c5e89e13b0
YashSam1 <130311450+YashSam1@users.noreply.github.com>
2024-07-13 18:36:14 -0400

    Update entrypoint.sh
    Absolute path to screenshots

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,11 +3,9 @@
 if [[ $# -eq 0 ]] ; then
     /usr/bin/X :0 -nocursor -nolisten tcp vt1
 else
-    mkdir -p tmp/screenshots
-    DISPLAY=:0
     # maximize the main window
     echo 'nohup bash -c "sleep 10; while true; do xdotool getwindowfocus windowsize $(xdotool getdisplaygeometry); sleep 60; done" &' > /usr/local/bin/entrypoint-command.sh
-    echo 'nohup bash -c "sleep 10; while true; do scrot -F tmp/screenshots/screen.png -o -z >> tmp/screenshots/scrot.log 2>&1; sleep 60; done" &' >> /usr/local/bin/entrypoint-command.sh
+    echo 'nohup bash -c "sleep 10; while true; do scrot -F /screenshots/screen.png -o -z >> /screenshots/scrot.log 2>&1; sleep 60; done" &' >> /usr/local/bin/entrypoint-command.sh
     echo $DISPLAY
 
     echo "$@" >> /usr/bin/entrypoint-command.sh
