c347afab485d6ac8db327e790fc5509507c10ac8
YashSam1 <130311450+YashSam1@users.noreply.github.com>
2024-10-26 16:10:59 -0400

    Update main.go - Adding certificates folder and mounting

--- main.go
+++ main.go
@@ -18,6 +18,7 @@ import (
 const rawContainerStoragePath = "/perm/container-storage"
 const tmpStoragePath = "/perm/temp-container"
 const tmpScreenshots = "/tmp/screenshots"
+const tmpCertificates = "/tmp/certificates"
 
 func main() {
 	logger.Print("starting up")
@@ -40,7 +41,7 @@ func main() {
 		"--device", "/dev/snd",
 		"--cap-add", "SYS_TTY_CONFIG",
 		"--mount", "type=bind,src=/tmp/screenshots,dst=/screenshots",
-		
+		"--mount", "type=bind,src=/tmp/certificates,dst=/certificates",
 	}
 
 	runArgs, gokrazyArgs := mergeArgs(defaultArgs, os.Args)
@@ -91,6 +92,11 @@ func run(cancel context.CancelFunc, errChan chan error, containerName string, ar
 
 	logger.Printf("Creating %s", tmpScreenshots)
 	if err := os.MkdirAll(path.Join(tmpScreenshots), os.ModePerm); err != nil {
+		logger.Fatal(err)
+	}
+
+	logger.Printf("Creating %s", tmpCertificates)
+	if err := os.MkdirAll(path.Join(tmpCertificates), os.ModePerm); err != nil {
 		logger.Fatal(err)
 	}
 
